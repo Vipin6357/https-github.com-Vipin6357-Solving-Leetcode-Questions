bda8e834c2c88db187b1224880cebc2749777d6f
Vipin Gangwar <Vipin63966345557@gmail.com>
2026-08-14 14:00:32 +0530

    Time: 0 ms (100%), Space: 42 MB (73.44%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        
+        if ( n == 0 ) return 1;
+
+        int mask = 0;
+        int x = n;
+        while( x > 0 ){
+            mask = ( mask << 1) | 1;
+            x >>= 1;
+        }
+        return n ^ mask;
+    }
+}
